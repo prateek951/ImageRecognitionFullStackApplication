88d0f61f1ef1b373fdf927327a3aed532dd4b6ae
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-08-15 17:53:39 +0530

    code does multiple face detection now

--- frfsapi/controllers/image.js
+++ frfsapi/controllers/image.js
@@ -1,23 +1,45 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 
-/*@desc API Key for the Clarifai Face Detection Model*/
+/* the Clarifai Face Detection Model*/
 const app = new Clarifai.App({
-    apiKey: "b36bbd677f724f3088c1fe38242d85ac"
-  });
+  apiKey: "fda1616d27d84e59ac9abc9262816fe4"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch(err =>
+      res
+        .status(400)
+        .json(
+          "unable to work with clarifai api.Please contact the admin or try again later" +
+            err
+        )
+    );
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => res.json(entries[0]))
+    .catch(err =>
+      res
+        .status(400)
+        .json(
+          "unable to work with the clarifai api.Please contact the admin or try again later" +
+            err
+        )
+    );
+};
 
 module.exports = {
-    handleApiCall: (req, res) => {
-        app.models
-        .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
-        .then(data => res.json(data))
-        .catch(err => res.status(400).json('unable to work with clarifai for some reason'));
-    },
-    handleImage: (req, res,db) => {
-        const {id} = req.body;
-        db('users').where('id','=',id)
-        .increment('entries',1)
-        .returning('entries')
-        .then(entries => res.json(entries[0]))
-        .catch(err => res.status(400).json('unable to get the entries'));
-    }
-}
+  handleApiCall,
+  handleImage
+};
